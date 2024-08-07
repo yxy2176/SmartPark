d250524fe920b31509b9b0d445ef8b998134e33b
JennyYao <344561707@qq.com>
2024-08-07 16:46:21 +0800

    day01

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { loginAPI } from "@/api/user";
-import { setToken } from "@/utils/auth";
+import { setToken,getToken } from "@/utils/auth";
 
 export default {
   // 命名空间，别忘了最后的d
